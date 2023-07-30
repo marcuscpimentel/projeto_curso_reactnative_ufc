3bbaf4fc8b629d95e6f986be6de4586a21075682
Marcus Chrystian Pimentel Alves <99371450+marcuscpimentel@users.noreply.github.com>
2023-07-30 14:21:52 -0300

    Atualizar código no arquivo App.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Lista from './Lista';
+import Header from './Header';
 
-export default function App() {
+const App = () => {
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Atividade Curso UFC" />
+      <Lista />
     </View>
+    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: '100%',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+export default App;
